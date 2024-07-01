bbd4ce76f787186a6ef375dbe9c53d075bf7002e
Fran Salanova <fsalanova.16@gmail.com>
2024-07-01 00:41:39 -0300

    Working in the parser for the insert command

--- parsers/insert.js
+++ parsers/insert.js
@@ -1,7 +1,7 @@
-const InsertCommand = require('../commands/InsertCommand')
+const InsertCommand = require("../commands/InsertCommand")
 
-const INSERT_COMMAND = 'INSERT'
-const BEFORE_TABLE_COMMAND = 'INTO'
+const INSERT_COMMAND = "INSERT"
+const BEFORE_TABLE_COMMAND = "INTO"
 
 const REGEX = new RegExp(
   `${INSERT_COMMAND}\\s+(?<record>{.*})\\s+${BEFORE_TABLE_COMMAND}\\s+(?<tableName>\\S+)`
@@ -11,10 +11,20 @@ function parseInsertCommand(commandString) {
   const regexMatch = commandString.match(REGEX)
   if (regexMatch == null) return
 
-  const record = JSON.parse(regexMatch.groups.record)
+  const record = safeParseJSON(regexMatch.groups.record)
+  if (record == null) return
+
   const tableName = regexMatch.groups.tableName
 
   return new InsertCommand({ record, tableName })
+}
+
+function safeParseJSON(string) {
+  try {
+    return JSON.parse(string)
+  } catch {
+    return
+  }
 }
 
 module.exports = parseInsertCommand
---
+++ parsers/insert.test.js
@@ -0,0 +1,44 @@
+const parseInsertCommand = require("./insert")
+
+describe("With a valid command", () => {
+  const command = 'INSERT { "a": 1 } INTO table'
+
+  test("It returns correct insert command", () => {
+    const insertCommand = parseInsertCommand(command)
+
+    expect(insertCommand.record).toEqual({ a: 1 })
+    expect(insertCommand.tableName).toBe("table")
+  })
+})
+
+describe("With an invalid record", () => {
+  const command = "INSERT { : 1 } INTO table"
+
+  test("It returns undefined", () => {
+    expect(parseInsertCommand(command)).toBeUndefined()
+  })
+})
+
+describe("With no table name", () => {
+  const command = 'INSERT { "a" : 1 } INTO'
+
+  test("It returns undefined", () => {
+    expect(parseInsertCommand(command)).toBeUndefined()
+  })
+})
+
+describe("With no INSERT clause", () => {
+  const command = '{ "a": 1 } INTO table'
+
+  test("It returns undefined", () => {
+    expect(parseInsertCommand(command)).toBeUndefined()
+  })
+})
+
+describe("With no INTO clause", () => {
+  const command = 'INSERT { "a": 1 } table'
+
+  test("It returns undefined", () => {
+    expect(parseInsertCommand(command)).toBeUndefined()
+  })
+})
